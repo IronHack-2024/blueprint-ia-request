b0469d767c48b92e3dbb41dd52e5bdd62608b5fd
Lorena Criado <criado.manzaneque@gmail.com>
2024-10-24 20:13:10 +0200

    Initial functional test for Gemini API connection

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(morgan('tiny'));
 
+app.get('/api', async (req, res) => {
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = "Generate a multiple-choice question suitable for junior developers about Node.js.";
+
+    const result = await model.generateContent(prompt);
+    res.json(result.response.text());
+})
 
 app.listen(PORT, () => {
-    
-    // connectDB();
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 })
